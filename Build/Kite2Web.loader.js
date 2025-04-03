63841e9846e8747abfa0cb30703ab2d5024ebcd5
S3nnar <privat.mc@t-online.de>
2025-04-03 10:48:03 +0200

    fixed copy buttons notification

--- Build/Kite2Web.loader.js
+++ Build/Kite2Web.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 80071962,
+    wasmFileSize: 80072038,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
